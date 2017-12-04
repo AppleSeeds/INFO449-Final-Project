e887b0f254430d3e9c920572d93ddcb1e4049ebd
Chao Ma <ynyounuo@me.com>
2017-12-04 00:59:51 -0800

    Initialize the main structure

---
+++ client/EatWithFriends/SearchFriendVC.swift
@@ -0,0 +1,35 @@
+//
+//  SearchFriendVC.swift
+//  EatWithFriends
+//
+//  Created by ​ on 12/4/17.
+//  Copyright © 2017 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class SearchFriendVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
