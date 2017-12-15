a3691369f956bec7aaa8120a03c73d12d769a424
Chao Ma <ynyounuo@me.com>
2017-12-14 16:36:02 -0800

    Add patch function examples

--- client/EatWithFriends/Model/SelfMode.swift
+++ client/EatWithFriends/Model/SelfMode.swift
@@ -12,24 +12,24 @@ class SelfMode {
     var allUsers: [User]
     var name: String
     var id: String
-    
+
     var fetchedFriend: [User]
     var fetchedFriendString: [String]
-    
+
     var foodLiked: [String]
     var foodHated: [String]
     var restLiked: [Restaurant]
     var restHated: [Restaurant]
     var restList: [Restaurant]
     var isRegistered: Bool
-    
+
     let semaphore = DispatchSemaphore(value: 0)
     let semaphore2 = DispatchSemaphore(value: 0)
 
     init() {
         self.name = LoginViewController.GlobalVariable.myFirstName
         self.id = LoginViewController.GlobalVariable.myUserId
-        
+
         self.allUsers = []
         self.fetchedFriend = []
         self.fetchedFriendString = []
@@ -39,42 +39,42 @@ class SelfMode {
         self.restHated = []
         self.restList = []
         self.isRegistered = false
-        
+
         var A = User(name: "A", foodLiked: [""], foodHated: [""], restLiked: [], restHated: [])
         var B = User(name: "B", foodLiked: [""], foodHated: [""], restLiked: [], restHated: [])
         var C = User(name: "C", foodLiked: [""], foodHated: [""], restLiked: [], restHated: [])
         var D = User(name: "D", foodLiked: [""], foodHated: [""], restLiked: [], restHated: [])
-        
+
         makeGetRestaurantRequest(url: "https://info449.com/uw-restaurants-info449")
         makeGetUserRequest(url: "https://info449.com/users-info449")
-        
+
         fetchedFriend.append(A)
         fetchedFriend.append(B)
         fetchedFriend.append(C)
         fetchedFriend.append(D)
-        
+
         foodLiked.append("Chinese")
         foodHated.append("Japanese")
         restLiked.append(Restaurant(name: "Test1", image_url: "", categories: [], rating: 1, price: "", address: ",", phone: "", latitude: 1.1, longitude: 1.1))
         restHated.append(Restaurant(name: "Test2", image_url: "", categories: [], rating: 1, price: "", address: ",", phone: "", latitude: 1.1, longitude: 1.1))
     }
-    
-    func getFetchedFriend() -> [User]{
+
+    func getFetchedFriend() -> [User] {
         return self.fetchedFriend
     }
-    
+
     func getFoodLiked() -> [String] {
         return self.foodLiked
     }
-    
+
     func getFoodHated() -> [String] {
         return self.foodHated
     }
-    
+
     func getRestLiked() -> [Restaurant] {
         return self.restLiked
     }
-    
+
     func getRestHated() -> [Restaurant] {
         return self.restHated
     }
@@ -82,67 +82,67 @@ class SelfMode {
     func getRestList() -> [Restaurant] {
         return self.restList
     }
-    
+
     func getAllUser() -> [User] {
         return self.allUsers
     }
-    
+
 ////////////////////////////////// Http connection
     func makeGetUserRequest(url: String) {
         guard let URL = URL(string: url) else { return }
-        let task = URLSession.shared.dataTask(with: URL) {(data, response, error) in
-                if (!Reachability.isConnectedToNetwork() || error != nil) {
-                    if (error != nil) {
-                        print(error ?? "Somthing wrong")
-                    }
-                } else{
-                    do {
-                        let json = try JSONSerialization.jsonObject(with: data!) as! [AnyObject]
-                        self.buildSelf(json: json, url: url)
-                    } catch {
-                        print (error)
-                    }
-                    
+        let task = URLSession.shared.dataTask(with: URL) { (data, response, error) in
+            if (!Reachability.isConnectedToNetwork() || error != nil) {
+                if (error != nil) {
+                    print(error ?? "Somthing wrong")
                 }
-                self.semaphore2.signal()
+            } else {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!) as! [AnyObject]
+                    self.buildSelf(json: json, url: url)
+                } catch {
+                    print (error)
+                }
+
+            }
+            self.semaphore2.signal()
         }
         task.resume()
         self.semaphore2.wait()
     }
-    
+
     private func buildSelf(json: [AnyObject], url: String) {
         for obj in json {
-            let user = obj as? [String:AnyObject]
+            let user = obj as? [String: AnyObject]
             let userName = user!["fullName"] as! String!
             let userEmail = user!["email"] as! String!
             let userId = user!["id"] as! String! // id might need to change
             let userFriendListString = user!["friend_list"] as! [String]!
-            
+
             var userCategoriesLiked = [String]()
             var userCategoriesHated = [String]()
             var userRestLikedString = [String]()
             var userRestHatedString = [String]()
-            
-            let preference = user!["preference"] as! [[String:[Any]]]!
-            let prefObj = preference![0] as! [String:[Any]]
-            
+
+            let preference = user!["preference"] as! [[String: [Any]]]!
+            let prefObj = preference![0] as! [String: [Any]]
+
             let resObj = prefObj["restaurants"] as! [Any]
             let catObj = prefObj["categories"] as! [Any]
-            
-            let restDic = resObj[0] as! [String:[String]]
-            let catDic = catObj[0] as! [String:[String]]
-            
+
+            let restDic = resObj[0] as! [String: [String]]
+            let catDic = catObj[0] as! [String: [String]]
+
             userCategoriesLiked = restDic["res_like"] as! [String]
             userCategoriesHated = restDic["res_dislike"] as! [String]
             userCategoriesLiked = catDic["cat_like"] as! [String]
             userCategoriesHated = catDic["cat_dislike"] as! [String]
-            
+
             let userRestLiked = findRest(restString: userRestLikedString)
             let userRestHated = findRest(restString: userRestHatedString)
-            
-            let userObj = User(name: userName!, id:userId!, email: userEmail!, foodLiked: userCategoriesLiked, foodHated: userCategoriesHated, restLiked: userRestLiked, restHated: userRestHated, friends: userFriendListString!)
+
+            let userObj = User(name: userName!, id: userId!, email: userEmail!, foodLiked: userCategoriesLiked, foodHated: userCategoriesHated, restLiked: userRestLiked, restHated: userRestHated, friends: userFriendListString!)
             allUsers.append(userObj)
-            
+
             if (userName == self.name && userId == self.id) {
                 isRegistered = true
                 self.fetchedFriendString = userFriendListString!
@@ -164,7 +164,7 @@ class SelfMode {
             makePostRequest(url: url)
         }
     }
-    
+
     private func findFriend(friendString: [String]) -> [User] {
         var result = [User]()
         for friend in friendString {
@@ -176,7 +176,7 @@ class SelfMode {
         }
         return result
     }
-    
+
     private func findRest(restString: [String]) -> [Restaurant] {
         var result = [Restaurant]()
         for rest in restString {
@@ -188,16 +188,16 @@ class SelfMode {
         }
         return result
     }
-    
+
     // get all the resuaurant information
     func makeGetRestaurantRequest(url: String) {
         let uRL = URL(string: url)
-        let task = URLSession.shared.dataTask(with: uRL!) {(data, response, error) in
+        let task = URLSession.shared.dataTask(with: uRL!) { (data, response, error) in
             if (!Reachability.isConnectedToNetwork() || error != nil) {
                 if (error != nil) {
                     print(error)
                 }
-            } else{
+            } else {
                 do {
                     let json = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves) as! [AnyObject]
                     self.buildRest(json: json)
@@ -210,87 +210,87 @@ class SelfMode {
         task.resume()
         semaphore.wait()
     }
-    
+
     func buildRest(json: [AnyObject]) {
         for obj in json {
-            let rest = obj as? [String:AnyObject]
-            
+            let rest = obj as? [String: AnyObject]
+
             let restName = rest!["name"] as! String
             let image_url = rest!["image_url"] as! String
             let categories = rest!["categories"] as! [AnyObject]!
             var categoryList = [String]()
-            
+
             for subCateObj in categories! {
-                let subCates = subCateObj as? [String:String]
+                let subCates = subCateObj as? [String: String]
                 let subCate = subCates!["title"] as String!
                 categoryList.append(subCate!)
             }
-            
+
             let rating = rest!["rating"] as! Double
             let price = rest!["price"] as! String
             let addressObj = rest!["location"] as? [String: AnyObject]
-            let addressDisplay = addressObj!["display_address"]  as! [String]
+            let addressDisplay = addressObj!["display_address"] as! [String]
             var address = ""
             for addressIndex in addressDisplay {
                 address += (addressIndex + " ")
             }
-            
+
             let phone = rest!["phone"] as! String
             let longitude = rest!["coordinates"]!["longitude"] as! Double
             let latitude = rest!["coordinates"]!["latitude"] as! Double
-            
+
             self.restList.append(Restaurant(name: restName, image_url: image_url, categories: categoryList, rating: rating, price: price, address: address, phone: phone, latitude: latitude, longitude: longitude))
         }
     }
-    
+
     // Post a new user
     func makePostRequest(url: String) {
         let param = ["fullName": self.name,
-                     "givenName":"",
-                     "familyName":"",
-                     "email": "",
-                     "id": self.id,
-                     "friend_list":[
-                        ""
-                     ],
-                     "preference": [
-                        [
-                            "categories": [
-                                ["cat_like":[
-                                    ""
-                                ],
-                                "cat_dislike":[
-                                    ""
-                                ]]
+            "givenName": "",
+            "familyName": "",
+            "email": "",
+            "id": self.id,
+            "friend_list": [
+                ""
+            ],
+            "preference": [
+                [
+                    "categories": [
+                        ["cat_like": [
+                            ""
                             ],
-                            "restaurants":[
-                                    ["res_like": [
-                                        ""
-                                    ],
-                                    "res_like_id": [
-                                        ""
-                                    ],
-                                    "res_dislike": [
-                                        ""
-                                    ],
-                                    "res_dislike_id": [
-                                        ""
-                                    ]
-                                ]
+                            "cat_dislike": [
+                                ""
+                            ]]
+                    ],
+                    "restaurants": [
+                        ["res_like": [
+                            ""
+                            ],
+                            "res_like_id": [
+                                ""
+                            ],
+                            "res_dislike": [
+                                ""
+                            ],
+                            "res_dislike_id": [
+                                ""
+                            ]
                         ]
-                        ]]
-                    ] as [String : Any]
-        
+                    ]
+                ]]
+        ] as [String: Any]
+
         guard let url = URL(string: url) else { return }
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        guard let httpBody = try? JSONSerialization.data(withJSONObject: param) else {return}
+
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: param) else { return }
         request.httpBody = httpBody
-        
+
         let session = URLSession.shared
-        session.dataTask(with: request) {(data, response, error) in
+        session.dataTask(with: request) { (data, response, error) in
             if let response = response {
                 print(response)
             }
@@ -304,9 +304,77 @@ class SelfMode {
             }
         }.resume()
     }
-    
+
     // change data of existing user
+    // identifier to determine change preference or change friendlist
     
-    
+    // pass param in forms like the example with whole list of friends and pereference;
+    func makeChanges (url: String, /*identifier: String,*/ param: [String:Any]) {
+
+//        if identifier == "friends" {
+//            let param = [
+//                "id": self.id,
+//                "friend_list": [
+//                    changes
+//                ]
+//            ] as [String: Any]
+//        } else {
+//            let param = [
+//                "id": self.id,
+//                "preference": [
+//                    changes
+//                        [
+//                            "categories": [
+//                                ["cat_like":[
+//                                    ""
+//                                    ],
+//                                 "cat_dislike":[
+//                                    ""
+//                                    ]]
+//                            ],
+//                            "restaurants":[
+//                                ["res_like": [
+//                                    ""
+//                                    ],
+//                                 "res_like_id": [
+//                                    ""
+//                                    ],
+//                                 "res_dislike": [
+//                                    ""
+//                                    ],
+//                                 "res_dislike_id": [
+//                                    ""
+//                                    ]
+//                                ]
+//                            ]
+//                        ]
+//                ]
+//            ] as [String: Any]
+//        }
+
+        guard let url = URL(string: url) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "PATCH"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: param) else { return }
+        request.httpBody = httpBody
+
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
+
 //////////////////////////////////////////////////////////////////////////////////////////
 }
